7451f292452c2f2d80d67b5c716f2a93df9539ad
Roman RBerendeev <roman.berendeev@rentateam.ru>
2019-03-02 21:42:35 +0300

    ellipse scale

--- app/src/main/java/ru/berendeev/roman/clock/view/ClockView.kt
+++ app/src/main/java/ru/berendeev/roman/clock/view/ClockView.kt
@@ -2,7 +2,6 @@ package ru.berendeev.roman.clock.view
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.support.annotation.RequiresApi
@@ -17,10 +16,15 @@ private const val CLOCK_STROKE_WIDTH = 4
 class ClockView : View {
 
     private val blackPaint: Paint = Paint()
-    private val transparentPaint: Paint = Paint()
+    private val blackFilledPaint: Paint = Paint()
     private val blackOvalRect = RectF()
     private val transparentOvalRect = RectF()
     private val hourRect = RectF()
+
+    private var px = 0f
+    private var py = 0f
+    private var effectiveWidth = 0
+    private var effectiveHeight = 0
 
     private var hoursWidth: Int = 0
     private var hoursLength: Int = 0
@@ -58,8 +62,8 @@ class ClockView : View {
         blackPaint.strokeWidth = CLOCK_STROKE_WIDTH.toFloat()
         blackPaint.style = Paint.Style.STROKE
 
-        transparentPaint.isAntiAlias = true
-        transparentPaint.color = Color.TRANSPARENT
+        blackFilledPaint.isAntiAlias = true
+        blackFilledPaint.color = ContextCompat.getColor(context, R.color.black)
 
 
     }
@@ -91,19 +95,60 @@ class ClockView : View {
         blackOvalRect.top = paddingTop.toFloat()
         blackOvalRect.bottom = h.toFloat() - paddingBottom
 
-        hourRect.left = 0f
-        hourRect.right = hoursLength.toFloat()
-        hourRect.top = h.toFloat()/2 + hoursWidth.toFloat()
-        hourRect.bottom = h.toFloat()/ 2 + hoursWidth.toFloat()
+        px = (paddingLeft + w - paddingRight).toFloat() / 2
+        py = (paddingTop + h - paddingBottom).toFloat() / 2
+        effectiveWidth = w - paddingLeft - paddingRight
+        effectiveHeight = h - paddingTop - paddingBottom
+
+        hourRect.left = -effectiveWidth.toFloat() / 2
+        hourRect.right = -effectiveWidth.toFloat() / 2 + hoursLength
+        hourRect.top = - hoursWidth.toFloat()
+        hourRect.bottom = hoursWidth.toFloat()
+
+//        hourRect.left = 0f
+//        hourRect.right = hoursLength.toFloat()
+//        hourRect.top = h.toFloat()/2 + hoursWidth.toFloat()
+//        hourRect.bottom = h.toFloat()/ 2 + hoursWidth.toFloat()
+
+
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawOval(blackOvalRect, blackPaint)
-        canvas.drawOval(transparentOvalRect, transparentPaint)
+        drawLines(canvas)
+        drawRects(canvas)
+    }
+
+    private fun drawHourDevision(canvas: Canvas) {
+        canvas.drawRect(hourRect, blackFilledPaint)
+    }
+
+    private fun drawMinuteDevision(canvas: Canvas) {
+        canvas.drawRect(hourRect, blackFilledPaint)
+    }
+
+    private fun drawRects(canvas: Canvas) {
         (0..12).forEach {
+            canvas.save()
+            canvas.scale(1f, 3f, px, py)
+            canvas.rotate(30 * it.toFloat(), px, py)
+            canvas.translate(px, py)
+            canvas.drawCircle(0f, 0f, 4f, blackPaint)
+            if (it % 5 == 0) {
+                drawHourDevision(canvas)
+            } else {
+                drawMinuteDevision(canvas)
+            }
+            canvas.restore()
+        }
+    }
+
+    private fun drawLines(canvas: Canvas) {
+        val linesCount = 12
+        (0..linesCount).forEach {
             val px = (paddingLeft + width - paddingRight).toFloat() / 2
             val py = (paddingTop + height - paddingBottom).toFloat() / 2
-            val alpha = PI / 6 * it
+            val alpha = 2 * PI / linesCount * it
             val radius = ovalRadius(alpha)
             val x1 = radius * cos(alpha)
             val y1 = radius * sin(alpha)
@@ -117,9 +162,10 @@ class ClockView : View {
         }
     }
 
-    private fun ovalRadius(alpha: Double): Float {
-        val a = (width - paddingLeft - paddingRight).toFloat() / 2
-        val b = (height - paddingTop - paddingBottom).toFloat() / 2
+    private fun ovalRadius(alphaDegrees: Double): Float {
+        val alpha = PI * alphaDegrees / 180
+        val a = effectiveWidth.toFloat() / 2
+        val b = effectiveWidth.toFloat() / 2
         val ab = a * b
         val a2 = a * a
         val b2 = b * b
